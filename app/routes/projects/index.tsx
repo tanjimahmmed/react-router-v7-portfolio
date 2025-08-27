3d31ee3fc4e79c14463c2187463206034593497f
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-08-27 11:08:08 +0600

    add fetch projects with loader

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import type { Route } from "./+types/index";
 
-const ProjectsPage = () => {
+export async function loader({request}: Route.LoaderArgs):Promise<any> {
+  const res = await fetch('http://localhost:8000/projects');
+  const data = await res.json();
+
+  return {projects: data};
+}
+
+const ProjectsPage = ({loaderData}: Route.ComponentProps) => {
+  const {projects} = loaderData;
+  console.log(projects);
+  
   return (
     <>
         <h2 className='text-3xl font-bold mb-8'>Projects</h2>

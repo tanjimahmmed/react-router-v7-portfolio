51632dc6e84c80b6329edbbbd6cceb12699ed2c6
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-08-27 11:57:48 +0600

    add projects on frontend

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,6 +1,8 @@
 import type { Route } from "./+types/index";
+import type { Project } from "~/types";
+import ProjectCard from "~/component/ProjectCard";
 
-export async function loader({request}: Route.LoaderArgs):Promise<any> {
+export async function loader({request}: Route.LoaderArgs):Promise<{projects: Project[]}> {
   const res = await fetch('http://localhost:8000/projects');
   const data = await res.json();
 
@@ -8,12 +10,18 @@ export async function loader({request}: Route.LoaderArgs):Promise<any> {
 }
 
 const ProjectsPage = ({loaderData}: Route.ComponentProps) => {
-  const {projects} = loaderData;
+  const {projects} = loaderData as {projects: Project[]};
   console.log(projects);
   
   return (
     <>
         <h2 className='text-3xl font-bold mb-8'>Projects</h2>
+
+        <div className="grid gap-6 sm:grid-cols-2">
+          {projects.map((project) => (
+            <ProjectCard key={project.id} project={project}/>
+          ))}
+        </div>
     </>
   )
 }

99eda32114c7461c2d4f896e348b231261be933c
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-08-29 16:08:44 +0600

    fetch post meta

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import type { Route } from "./+types/index";
+import type { PostMeta } from "~/types";
+import {Link} from "react-router";
 
-const BlogPage = () => {
+export async function loader({request}: Route.LoaderArgs): Promise<{posts: PostMeta[]}> {
+  const url = new URL('/posts-meta.json', request.url);
+  const res = await fetch(url.href);
+
+  if(!res.ok) throw new Error('Failed to fetch data')
+
+  const data = await res.json();
+
+  return {posts: data}
+}
+
+const BlogPage = ({loaderData}: Route.ComponentProps) => {
+  const {posts} = loaderData;
+
+  console.log(posts);
+  
   return (
     <>
         <h2 className='text-3xl font-bold mb-8'>Blog</h2>

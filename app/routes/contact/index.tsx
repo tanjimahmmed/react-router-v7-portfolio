f83b19d5080a157bc9aa4e97405911112027f7e3
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-08-29 12:52:16 +0600

    react router action using contact form

--- app/routes/contact/index.tsx
+++ app/routes/contact/index.tsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import type { Route } from "./+types";
+import { Form } from "react-router";
 
-const ContactPage = () => {
+export async function action({request}: Route.ActionArgs) {
+  const formData = await request.formData();
+  const name = formData.get('name');
+  const email = formData.get('email');
+  const subject = formData.get('subject');
+  const message = formData.get('message');
+  const data = {
+    name,
+    email,
+    subject,
+    message
+  };
+  return {message: 'Form submitted successfully', data};
+}
+
+const ContactPage = ({actionData}: Route.ComponentProps) => {
   return (
     <div className='max-w-3xl mx-auto mt-12 px-6 py-8 bg-gray-900'>
       <h2 className='text-3xl font-bold text-white mb-8 text-center'>
          📬 Contact Me
       </h2>
-      <form action="" className='space-y-6'>
+
+      {actionData?.message ? (<p className="mb-6 p-4 bg-green-700 text-green-100 text-center rounded-lg border border-green-500 shadow-md">{actionData.message}</p>) : null}
+      <Form method="post" className='space-y-6'>
         <div className="">
           <label htmlFor="name" className='block text-sm font-medium text-gray-300'>Full Name</label>
 
@@ -30,7 +48,7 @@ const ContactPage = () => {
           <textarea id='message' name='message' className='w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100' />
         </div>
         <button className='w-full bg-blue-600 hover:bg-blue-700 cursor-pointer text-white py-2 rounded-lg'>Send Message</button>
-      </form>
+      </Form>
     </div>
   )
 }

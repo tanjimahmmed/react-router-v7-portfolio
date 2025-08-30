8f8be0ba8e513302b9063a0869d58eb44277db47
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-08-30 14:13:01 +0600

    add featured post on homepage

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,7 +1,9 @@
 import AboutPreview from "~/component/AboutPreview";
 import type { Route } from "../+types/index";
 import FeaturedProjects from "~/component/FeaturedProjects";
+import LatestPosts from "~/component/LatestPosts";
 import type { Project } from "~/types";
+import type { PostMeta } from "~/types";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,20 +12,37 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export async function loader({request}: Route.LoaderArgs): Promise<{projects: Project[]}>{
-  const res = await fetch(`${import.meta.env.VITE_API_URL}/projects`);
-  const data = await res.json();
-  return {projects: data};
+export async function loader({request}: Route.LoaderArgs): Promise<{projects: Project[]; posts: PostMeta[]}>{
+
+  const url = new URL(request.url);
+
+  const [projectRes, postRes] = await Promise.all([
+    fetch(`${import.meta.env.VITE_API_URL}/projects`),
+    fetch(new URL(`/posts-meta.json`, url))
+  ]);
+
+  if(!projectRes.ok || !postRes.ok){
+    throw new Error('Failed to fetch projects or posts');
+  }
+
+  const [projects, posts] = await Promise.all([
+    projectRes.json(),
+    postRes.json()
+  ]);
+
+  
+  
+  
+  return {projects, posts};
 }
 
 const HomePage = ({loaderData}: Route.ComponentProps) => {
-  const {projects} = loaderData;
-
-  console.log(projects);
+  const {projects, posts} = loaderData;
   
   return <>
     <FeaturedProjects projects={projects} count={2}/>
     <AboutPreview/>
+    <LatestPosts posts={posts}/>
   </>
 }
 

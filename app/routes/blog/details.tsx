e2e0a41f1a100852b414eeadd5e4e5d9b3f80b01
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-08-30 11:55:20 +0600

    add display details page

--- app/routes/blog/details.tsx
+++ app/routes/blog/details.tsx
@@ -1,6 +1,7 @@
 import ReactMarkdown from 'react-markdown';
 import type {Route} from './+types/details';
 import type { PostMeta } from '~/types';
+import { Link } from 'react-router';
 
 export async function loader({request, params}: Route.LoaderArgs){
     const {slug} = params;
@@ -38,7 +39,16 @@ const BlogPostDetailsPage = ({loaderData}: BlogPostDetailsPageProps) => {
     console.log(postMeta, markdown);
     
   return (
-    <div>details</div>
+    <div className='max-w-3xl mx-auto px-6 py-12 bg-gray-900 text-white'>
+        <h1 className='text-3xl font-bold text-blue-400 mb-2'>
+            {postMeta.title}
+        </h1>
+        <p className='text-sm text-gray-400 mb-6'>{new Date(postMeta.date).toLocaleDateString()}</p>
+        <div className="prose prose-invert max-w-none mb-12">
+            <ReactMarkdown>{markdown}</ReactMarkdown>
+        </div>
+        <Link to='/blog' className='inline-block bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition'>Back to Posts</Link>
+    </div>
   )
 }
 
